f01496bc5703f5ce71ceabee8592d60415c3c5e2
DuyLe <minhduy4520@gmail.com>
2022-09-26 11:10:49 -0500

    update

--- Project 1/test/main/java/org/apache/commons/mail/EmailMock.java
+++ Project 1/test/main/java/org/apache/commons/mail/EmailMock.java
@@ -2,6 +2,7 @@ package org.apache.commons.mail;
 
 //send email message without attachments
 public class EmailMock extends Email{
+	private String sent;
 	
 	@Override
 	public Email setMsg(String msg) throws EmailException{
@@ -10,6 +11,26 @@ public class EmailMock extends Email{
 		}
 		setContent(msg, EmailConstants.TEXT_PLAIN);
 		return this;
+	}
+	
+	public String getSent() {
+		return sent;
+	}
+	
+	public String sendMimeMessage() throws EmailException{
+		EmailUtils.notNull(this.message, "MimeMessage has not been created yet");
+		try {
+			this.sent=this.message.getSubject();
+			return this.message.getMessageID();
+		}
+		catch(Throwable t) {
+			String msg="Sending the email to the following server failed : " + this.getHostName() + ":"+ this.getSmtpPort();
+			throw new EmailException(msg, t);
+		}
+	}
+	
+	public String getCharset() {
+		return charset;
 	}
 	
 }
--- Project 1/test/main/java/org/apache/commons/mail/EmailTest.java
+++ Project 1/test/main/java/org/apache/commons/mail/EmailTest.java
@@ -7,7 +7,7 @@ import org.apache.commons.mail.SimpleEmail;
 import junit.framework.TestCase;
 
 public class EmailTest extends TestCase{
-	Email testEmail;
+	EmailMock testEmail;
 	public void setUp() {
 		testEmail = new EmailMock();
 	}
@@ -60,14 +60,77 @@ public class EmailTest extends TestCase{
 		assertEquals(null, testEmail.contentType);
 	}
 	public void testUpdateContentTypeWithCharSet() throws EmailException{
-		testEmail.updateContentType("text/plain; charset=UTF-8");
-		assertEquals("text/plain; charset=UTF-8", testEmail.contentType);
+		testEmail.updateContentType("text/plain; charset=utf-8");
+		assertEquals("utf-8", testEmail.getCharset());
 	}
 	
 	public void testUpdateContentTypeWithoutCharSet() throws EmailException{
 		testEmail.updateContentType("text/plain");
 		assertEquals("text/plain", testEmail.contentType);
 	}
+	
+	public void testSend() throws EmailException{
+		testEmail.setHostName("b.com");
+		testEmail.setFrom("a@b.com");
+		testEmail.addTo("c@b.com");
+		testEmail.setSubject("a subject");
+		testEmail.send();
+		assertEquals("a subject", testEmail.getSent());
+	}
+	
+	public void testBuildMimeMessageNoHost() throws EmailException{
+		try {
+			testEmail.buildMimeMessage(); fail("Should throw EmailException");
+		} 
+		catch (EmailException e){
+			assertEquals("Cannot find valid hostname for mail session", e.getMessage());
+		}
+	}
+	
+	public void testBuildMimeMessageNoFrom() throws EmailException{
+		testEmail.setHostName("b.com");
+		try {
+			testEmail.buildMimeMessage(); fail("Should throw EmailException!");
+		}
+		catch(EmailException e) {
+			assertEquals("From address required", e.getMessage());
+		}
+	}
+	
+	public void testBuildMimeMessageNoTo() throws EmailException{
+		testEmail.setHostName("b.com");
+		testEmail.setFrom("a@b.com");
+		try {
+			testEmail.buildMimeMessage(); fail("Should throw EmailException!");
+		}
+		catch(EmailException e) {
+			assertEquals("At least one receiver address required", e.getMessage());
+		}
+	}
+	
+	public void testBuildMimeMessageExisting() throws EmailException{
+		testEmail.setHostName("b.com");
+		testEmail.setFrom("a@b.com");
+		testEmail.addTo("c@b.com");
+		testEmail.buildMimeMessage();
+		try {
+			testEmail.buildMimeMessage(); fail("Should throw illegal state exception");
+		}
+		catch(IllegalStateException e) {
+			assertEquals("The MimeMessage is already built.", e.getMessage());
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
